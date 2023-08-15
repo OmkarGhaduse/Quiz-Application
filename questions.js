5779c54c407a5c56f21981bab60b0b36434a937a
OmkarGhaduse <91007492+OmkarGhaduse@users.noreply.github.com>
2023-08-15 14:23:40 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+      numb: 1,
+      question: "How can you include external CSS in an HTML document?",
+      answer: "Using the link element",
+      options: [
+        "Using the style element",
+        "Using the css element",
+        "Using the link element",
+        "Using the external-css element"
+      ]
+    },
+      {
+      numb: 2,
+      question: "What does HTML stand for?",
+      answer: "Hyper Text Markup Language",
+      options: [
+        "Hyper Text Markup Language",
+        "Hyper Text Preprocessor",
+        "Hyper Text Multiple Language",
+        "Hyper Tool Multi Language"
+      ]
+    },
+    {
+      numb: 3,
+      question: "Which keyword is used to declare a variable in JavaScript?",
+      answer: "var",
+      options: [
+        "var",
+        "variable",
+        "v",
+        "let"
+      ]
+    },
+      {
+      numb: 4,
+      question: "What does CSS stand for?",
+      answer: "Cascading Style Sheet",
+      options: [
+        "Common Style Sheet",
+        "Colorful Style Sheet",
+        "Computer Style Sheet",
+        "Cascading Style Sheet"
+      ]
+    },
+    {
+      numb: 5,
+      question: "Which built-in function is used to display a dialog box with a message and an 'OK' button in JavaScript",
+      answer: "alert()",
+      options: [
+        "alert()",
+        "confirm()",
+        "prompt()",
+        "message()"
+      ]
+    }
+      
+  ];
